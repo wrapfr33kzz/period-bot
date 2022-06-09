a0b481da3bf9f7090768de750863a333b927ff94
Robii <92801274+wrapfr33kzz@users.noreply.github.com>
2022-06-09 11:17:03 +0530

    main file
    still in working stage

---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+void intro()
+{
+	cout<<"______________________________welcome aqiii___________________________________________"<<endl;
+	cout<<"#"<<endl;
+	Sleep(300);
+	cout<<"##"<<endl;
+	Sleep(100);
+	cout<<"###"<<endl;
+	Sleep(100);
+	cout<<"####"<<endl;
+	Sleep(100);
+	cout<<"#####"<<endl;
+	Sleep(100);
+	cout<<"######"<<endl;
+	Sleep(100);
+	cout<<"########"<<endl;
+	Sleep(100);
+	cout<<"###########"<<endl;
+	Sleep(100);
+	cout<<"#############"<<endl;
+	Sleep(100);
+	cout<<"################"<<endl;
+		Sleep(100);
+	cout<<"##################"<<endl;
+		Sleep(100);
+	cout<<"######################"<<endl;
+		Sleep(100);
+	cout<<"###########################"<<endl;
+		Sleep(100);
+	cout<<"#############################"<<endl;
+		Sleep(100);
+	cout<<"##################################"<<endl;
+		Sleep(100);
+	cout<<"######################################"<<endl;
+		Sleep(100);
+	cout<<"##################################################################"<<endl;
+			Sleep(100);
+	cout<<"#######################################################################"<<endl;
+			Sleep(100);
+	cout<<"#############################################################################"<<endl;
+			Sleep(100);
+	cout<<"################################################################################"<<endl;
+			Sleep(100);
+	cout<<"###################################################################################"<<endl;
+			Sleep(100);
+	cout<<"########################################################################################"<<endl;
+			Sleep(100);
+	cout<<"###########################################################################################"<<endl;
+			Sleep(100);
+	cout<<"#################################################################################################"<<endl;
+			Sleep(100);
+	cout<<"######################################################################################################"<<endl;
+				Sleep(100);
+	cout<<"############################################################################################################"<<endl;
+				Sleep(100);
+	cout<<"#################################################################################################################"<<endl;
+	Sleep(100);
+	cout<<"loading please wait aqi please";
+	Sleep(3000);
+	system("cls");
+}
+
+void dec()
+{
+	cout<<"aqi select your option and enter"<<endl;
+	Sleep(100);
+	cout<<"1)you feel stomach pain then enter 1"<<endl;;
+	Sleep(100);
+	cout<<"2)if dont feel then enter 2"<<endl;
+}
+void pain1()
+{
+	Sleep(100);
+	cout<<"__________________________________________________________________"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<" ";
+	cout<<"let me know how much its paining first"<<endl;
+	Sleep(100);
+	cout<<endl<<"@"<<endl;
+	cout<<"1)its paning very much "<<endl;
+	Sleep(100);
+	cout<<"2)its paining not that much just a bit"<<endl;
+}
+void pain2()
+{
+	cout<<"\n \n ";
+	Sleep(200);
+	cout<<"Aqiiiiiiiiiii"<<endl;
+	cout<<"\n first of all its ok ill suggest some remedies that help you"<<endl;
+	Sleep(100);
+	cout<<"cure this pain "<<endl;
+	Sleep(100);
+	
+}
+void remedy()
+{
+	cout<<"\n \n ";
+	cout<<"aqiiiiiiiii"<<endl;
+	Sleep(100);
+	cout<<"to see and selcet remedy please enter \n that options number"<<endl;
+	cout<<"other wise code doesn't work"<<endl;
+	Sleep(100);
+	
+	
+}
+
+main()
+{
+	intro();
+	dec();
+	int first_q;
+	cin>>first_q;
+	if(first_q==1)
+	{
+		pain1();
+		int option_pain2;
+		cin>>option_pain2;
+		if(option_pain2<=1)
+		{
+			pain2();
+			
+			{
+				
+			}
+			
+		}
+		
+	}else
+	{
+		
+	}
+	
+	
+}
